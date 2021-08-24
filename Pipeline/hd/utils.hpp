1caa10ef4019893691d06836e22be08e7d4bc54e
fxzjshm <fxzjshm@163.com>
2021-08-24 18:03:10 +0800

    Remove SYCL-PRNG and add MWC64X

--- Pipeline/hd/utils.hpp
+++ Pipeline/hd/utils.hpp
@@ -3,23 +3,9 @@
 
 #include <boost/compute.hpp>
 
-#include <PRNG/MWC64X.hpp>
 #include <type_traits>
 
 #include "hd/types.h"
-
-typedef prng::mwc64x_32 random_engine;
-template <typename T>
-struct uniform_int_distribution {
-    T min_value;
-    T max_value;
-
-    uniform_int_distribution(T m, T M) : min_value(m), max_value(M) {}
-
-    T operator()(random_engine rng) {
-        return (rng() % (max_value - min_value)) + min_value;
-    }
-};
 
 template <typename T, typename A1, typename A2>
 void device_to_host_copy(boost::compute::vector<T, A2> &d,
