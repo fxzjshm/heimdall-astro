545d3eddef6a7c78505bd07455d29531345144aa
fxzjshm <fxzjshm@163.com>
2021-09-02 16:11:12 +0800

    Add queue paramenter in device_vector_wrapper.dp.hpp

--- Pipeline/hd/utils/device_vector_wrapper.dp.hpp
+++ Pipeline/hd/utils/device_vector_wrapper.dp.hpp
@@ -3,6 +3,7 @@
 #include <boost/compute/algorithm/iota.hpp>
 #include <boost/compute/allocator/buffer_allocator.hpp>
 #include <boost/compute/container/vector.hpp>
+#include <boost/compute/system.hpp>
 
 template <class T, class Alloc = boost::compute::buffer_allocator<T>>
 class device_vector_wrapper : public boost::compute::vector<T, Alloc> {
@@ -16,15 +17,16 @@ public:
         return boost::compute::vector<T, Alloc>::operator=(v);
     }
 
-    inline void resize(size_type size) {
-        boost::compute::vector<T, Alloc>::resize(size);
+    inline void resize(size_type size, boost::compute::command_queue queue = boost::compute::system::default_queue()) {
+        boost::compute::vector<T, Alloc>::resize(size, queue);
     }
 
-    void resize(size_type new_size, const T &x) {
+    void resize(size_type new_size, const T &x, boost::compute::command_queue queue = boost::compute::system::default_queue()) {
         size_type old_size = boost::compute::vector<T, Alloc>::size();
         boost::compute::vector<T, Alloc>::resize(new_size);
         if(old_size < new_size){
             boost::compute::iota(boost::compute::vector<T, Alloc>::begin() + old_size, boost::compute::vector<T, Alloc>::end(), x);
+            queue.finish();
         }
     }
 };
