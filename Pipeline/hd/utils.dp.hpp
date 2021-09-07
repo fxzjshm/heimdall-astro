43717e01b0b9423cad4073c2973f725fe2f0062c
fxzjshm <fxzjshm@163.com>
2021-09-07 16:10:37 +0800

    Update utils.dp.hpp

--- Pipeline/hd/utils.dp.hpp
+++ Pipeline/hd/utils.dp.hpp
@@ -1,14 +1,17 @@
 // third-party code should be used with their corresponding licenses
 #pragma once
 
+#include "hd/utils/argument_wrapper.dp.hpp"
 #include "hd/utils/buffer_iterator.dp.hpp"
 #include "hd/utils/copy_if.dp.hpp"
 #include "hd/utils/device_to_host_copy.dp.hpp"
-#include "hd/utils/func_with_source_string.dp.hpp"
 #include "hd/utils/external_function.dp.hpp"
+#include "hd/utils/func_with_source_string.dp.hpp"
+#include "hd/utils/meta_kernel.dp.hpp"
 #include "hd/utils/operators.dp.hpp"
 #include "hd/utils/reduce_by_key_with_scan.dp.hpp"
 #include "hd/utils/reduce_by_key.dp.hpp"
+#include "hd/utils/scan_on_gpu.dp.hpp"
 #include "hd/utils/scatter_if.dp.hpp"
 #include "hd/utils/transform_if.dp.hpp"
 #include "hd/utils/type_defines.dp.hpp"
